aa10047117b8bb8a21edc18a1c5b8ed06f856fbd
shubranshumishra22 <shubranshumishra22@gmail.com>
2024-08-14 10:41:11 +0530

    user and video models completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
                 //making of userSchema
 
@@ -19,7 +21,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -48,6 +50,45 @@ const userSchema = new Schema(
     },{timestamps: true}
 )
 
-                //making of mongoose model
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password  ")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expireIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expireIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+                //making of mongoose model and giving reference of Schema to mongoose.model
 
 export const User = mongoose.model("User", userSchema)
